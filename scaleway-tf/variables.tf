a5056b732f46f8a866f4c4532a02490496f84644
jdvr <juandavidvegarguez@gmail.com>
2023-04-28 18:34:27 +0100

    fix(terraform): confgiure scw cred using tfvars

--- scaleway-tf/variables.tf
+++ scaleway-tf/variables.tf
@@ -2,3 +2,13 @@ variable "scw_project_id" {
   type = string
   description = "Scaleway project id. Project Dashboard > Settings"
 }
+
+variable "scw_access_key" {
+  type = string
+  description = "Scaleway credetials access key"
+}
+
+variable "scw_secret_key" {
+  type = string
+  description = "Scaleway credetials secret key"
+}
